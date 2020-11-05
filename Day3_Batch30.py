75ce8287d457fe54fa35ac4a221685e7a6b1f3f1
itshemant <itshemant@gmail.com>
2020-11-05 21:41:44 +0530

    Add files via upload

---
+++ Day3_Batch30.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name2 = 'ayan'
+print(name2)
+
+
+# In[3]:
+
+
+type(name2)
+
+
+# In[4]:
+
+
+fullname = 'harsh agarwal'
+print(fullname)
+
+
+# In[5]:
+
+
+print(fullname.title())
+
+
+# In[6]:
+
+
+#make upper case letters
+print(fullname.upper())
+
+
+# In[7]:
+
+
+#make lower case
+print(fullname.lower())
+
+
+# In[10]:
+
+
+#introduction to f string in python
+firstname = 'aliza'
+lastname = 'rehman'
+fullname = f"{firstname} {lastname}"
+print(fullname)
+
+
+# In[11]:
+
+
+print(fullname.title())
+
+
+# In[18]:
+
+
+# appreciate the practise files
+x = f"keep up the good work, {fullname}"
+print(x)
+
+
+# In[19]:
+
+
+#adding  a new line delimiter ====> \n
+print('favlang:\npython\ne\njava\nc++')
+
+
+# In[ ]:
+
+
+
+
